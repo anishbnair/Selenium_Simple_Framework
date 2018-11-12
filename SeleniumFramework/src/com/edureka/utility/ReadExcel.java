a02f976685a0447a8f4b21eee784723bcef471e6
anishbnair <nbanish@gmail.com>
2018-11-12 15:48:15 -0500

    created getData method

--- SeleniumFramework/src/com/edureka/utility/ReadExcel.java
+++ SeleniumFramework/src/com/edureka/utility/ReadExcel.java
@@ -3,6 +3,7 @@ package com.edureka.utility;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -13,7 +14,11 @@ public class ReadExcel {
 		File file = new File(filename);
 		FileInputStream fin = new FileInputStream(file);
 		Workbook wb = new XSSFWorkbook();
+		Sheet sh = wb.getSheet(sheetname);
+		int rowNum = sh.getLastRowNum();
+		int colNum = sh.getRow(0).getLastCellNum();
 		
+				
 		
 	}
 
