899b5908117fdbc0f4ef44714070d0bcff560ac2
anishbnair <nbanish@gmail.com>
2018-11-12 15:41:25 -0500

    added Apache POI jar files

--- SeleniumFramework/src/com/edureka/utility/ReadExcel.java
+++ SeleniumFramework/src/com/edureka/utility/ReadExcel.java
@@ -1,5 +1,20 @@
 package com.edureka.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ReadExcel {
+	
+	public static String[][] getData(String filename,String sheetname) throws Exception {
+		
+		File file = new File(filename);
+		FileInputStream fin = new FileInputStream(file);
+		Workbook wb = new XSSFWorkbook();
+		
+		
+	}
 
 }
