827aadf95004d48d2fe6dadffc20810aeb4ef007
anishbnair <nbanish@gmail.com>
2018-11-12 16:12:06 -0500

    created a class called LoginScript

--- SeleniumFramework/src/com/edureka/testScripts/LoginScript.java
+++ SeleniumFramework/src/com/edureka/testScripts/LoginScript.java
@@ -1,5 +1,25 @@
 package com.edureka.testScripts;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
 public class LoginScript {
+	
+	public WebDriver driver = null;
+	
+	@BeforeMethod
+	public void OpenBrowser() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://www.edureka.co/");
+		
+	}
 
 }
