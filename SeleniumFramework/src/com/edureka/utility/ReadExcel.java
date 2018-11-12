9111b49cc7c47ead137510cf49c97b421cd12690
anishbnair <nbanish@gmail.com>
2018-11-12 15:54:50 -0500

    completed coding for ReadExcel utility class

--- SeleniumFramework/src/com/edureka/utility/ReadExcel.java
+++ SeleniumFramework/src/com/edureka/utility/ReadExcel.java
@@ -3,6 +3,9 @@ package com.edureka.utility;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -18,8 +21,17 @@ public class ReadExcel {
 		int rowNum = sh.getLastRowNum();
 		int colNum = sh.getRow(0).getLastCellNum();
 		
-				
+		String[][] data = new String[rowNum][colNum];
 		
+		for(int i = 0; i < rowNum; i++) {
+			Row row = sh.getRow(i);
+			for(int j = 0; j < colNum; j++) {
+				Cell cell = row.getCell(j);
+				String value = new DataFormatter().formatCellValue(cell);
+				data[i][j] = value;				
+			}
+		}	
+		return data;
 	}
-
+	
 }
